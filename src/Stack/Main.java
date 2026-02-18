b80867976ba99e047a7113f26d9262b1fd6528cc
vinoth9598 <pvinoth9598@gmail.com>
2026-02-18 12:42:55 +0530

    Stack finish

--- src/Stack/Main.java
+++ src/Stack/Main.java
@@ -2,8 +2,17 @@ package Stack;
 
 public class Main {
     public static void main(String[] args){
-        Stacks myStack = new Stacks(4);
+        Stacks myStack = new Stacks(5);
 
+        myStack.getTop();
+        myStack.getHeight();
+
+        myStack.push(10);
+        myStack.push(15);
+        myStack.push(20);
+        myStack.push(25);
+
+        myStack.pop();
         myStack.printStack();
     }
 }
